05837fd787b12cc5c009d7e532447878e17d671a
Tobias Davis <accounts@davistobias.com>
2020-01-30 21:43:18 -0600

    fix: make rollup node modules explicit

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
+const external = [ 'crypto', 'url' ]
+
 module.exports = [
 	{
 		input: 'src/index-node.js',
 		output: {
 			file: 'dist/sign-aws-requests.js',
 			format: 'cjs'
-		}
+		},
+		external
 	},
 	{
 		input: 'src/index-node.js',
 		output: {
 			file: 'dist/sign-aws-requests.mjs',
 			format: 'es'
-		}
+		},
+		external
 	},
 	{
 		input: 'src/index-browser.js',
